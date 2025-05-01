2f37f5cb0810d34e7b7f1734988958d96713c835
dange084 <dange084@d.umn.edu>
2025-05-01 14:42:26 -0500

    start job home page

--- lib/jobHomePage.dart
+++ lib/jobHomePage.dart
@@ -1,2 +1,12 @@
 import 'package:flutter/material.dart';
 
+class JobHomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        
+      ),
+    );
+  }
+}
