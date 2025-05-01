f983468b1118a7e490a9409d1298e4d32a3ab549
dange084 <dange084@d.umn.edu>
2025-05-01 15:13:14 -0500

    added job home page

--- lib/jobHomePage.dart
+++ lib/jobHomePage.dart
@@ -2,11 +2,24 @@ import 'package:flutter/material.dart';
 
 class JobHomePage extends StatelessWidget{
   @override
+  _JobHomePageState createState() => _JobHomePageState();
+
+  @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-
+        backgroundColor: Colors.grey,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Welcome to your Job Search!"),
+          ],
+        )
       ),
     );
   }
+}
+
+class _JobHomePageState extends JobHomePage{
+
 }
