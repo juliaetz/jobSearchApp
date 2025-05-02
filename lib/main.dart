9df697fd36a41415113bf25eb7874c5d5aa137b2
ols00068 <ols00068@d.umn.edu>
2025-05-02 16:38:31 -0500

    Added Firebase firestore to our app

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:final_project/VIEW/jobHomePage.dart';
 import 'package:final_project/VIEW/darkTheme.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
