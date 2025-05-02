d07cdd20e20911b84212bb4b157e374d21cc4ec6
ols00068 <ols00068@d.umn.edu>
2025-05-02 18:25:09 -0500

    Fixed naming of files and classes to correctly refer to the Jobs and Salaries data set

--- lib/presenter/filter_data_science_presenter.dart
+++
@@ -1,8 +0,0 @@
-import '../model/filter_data_science_model.dart';
-
-class FilterDataSciencePresenter{
-  FilterDataSciencePresenter({required this.model});
-  final FilterDataScienceModel model;
-
-
-}
