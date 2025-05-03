2b9e9fe7ba6d1a4c544cb14a7078bddda2d727ae
ols00068 <ols00068@d.umn.edu>
2025-05-02 23:42:27 -0500

    Fixed the Data Science Page to switch between the list, filter, and search pages.

--- lib/VIEW/dataScienceJobsPage.dart
+++ lib/VIEW/dataScienceJobsPage.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import '../PRESENTER/load_data.dart';
 import '../MODEL/data_read.dart';
-
-class DataScienceJobsPage extends StatefulWidget {
+import 'filter_jobs_in_data_view.dart';
+class DataScienceListView extends StatefulWidget {
   @override
-  _DataScienceJobsPageState createState() => _DataScienceJobsPageState();
-}
+  _DataScienceListViewState createState() => _DataScienceListViewState();
 
-class _DataScienceJobsPageState extends State<DataScienceJobsPage> {
+}
+class _DataScienceListViewState extends State<DataScienceListView> {
   final repo = DataJobRepository();
   List<DataJob> jobs = [];
 
@@ -23,12 +23,10 @@ class _DataScienceJobsPageState extends State<DataScienceJobsPage> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    debugPrint('🔍 Building with jobs.length = ${jobs.length}');
-    return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.green, title: Text("Data Science Careers")),
-      body: FutureBuilder<List<DataJob>>(
+    @override
+    Widget build(BuildContext context) {
+      debugPrint('🔍 Building with jobs.length = ${jobs.length}');
+      return FutureBuilder<List<DataJob>>(
         future: repo.loadAndSort(),
         builder: (context, snapshot) {
           if (snapshot.connectionState != ConnectionState.done) {
@@ -50,6 +48,48 @@ class _DataScienceJobsPageState extends State<DataScienceJobsPage> {
             },
           );
         },
+      );
+    }
+  }
+class DataScienceJobsPage extends StatefulWidget {
+  @override
+  _DataScienceJobsPageState createState() => _DataScienceJobsPageState();
+}
+
+class _DataScienceJobsPageState extends State<DataScienceJobsPage> {
+
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _widgetOptions = <Widget>[
+    DataScienceListView(),
+    FilterJobsInDataView(),
+    Text("SearchJobsInDataViewHere"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.green, title: Text("Data Science Careers")),
+      body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'List'),
+          BottomNavigationBarItem(icon: Icon(Icons.filter_list), label: 'Filter'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
