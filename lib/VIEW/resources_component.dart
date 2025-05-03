fb2da5aee86ab611957419d1c83f132b9c4f37fe
etzko009 <etzko009@d.umn.edu>
2025-05-03 13:59:09 -0500

    adding resource page UI and ability to click links through pictures

--- lib/VIEW/resources_component.dart
+++ lib/VIEW/resources_component.dart
@@ -1,6 +1,13 @@
+import 'dart:io';
+
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:url_launcher/link.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
 import 'package:final_project/PRESENTER/resources_presenter.dart';
 import 'resources_view.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 
 class ResourcesPage extends StatefulWidget {
   final ResourcesPresenter presenter;
@@ -12,6 +19,20 @@ class ResourcesPage extends StatefulWidget {
   _ResourcesPageState createState() => _ResourcesPageState();
 }
 
+class articleInfo{
+  String URL = "example.com";
+  String articleTitle = "";
+  String website = "";
+  String imageAsset = "";
+
+  articleInfo(String url, String title, String web, String image){
+    URL = url;
+    articleTitle = title;
+    website = web;
+    imageAsset = image;
+  }
+}
+
 
 
 class _ResourcesPageState extends State<ResourcesPage> implements ResourcesView {
@@ -21,8 +42,20 @@ class _ResourcesPageState extends State<ResourcesPage> implements ResourcesView 
     this.widget.presenter.resourcesView = this;
   }
 
+  final DraggableScrollableController sheetController = DraggableScrollableController();
+  late InAppWebViewController inAppWebViewController;
+
+  final Set<Factory<OneSequenceGestureRecognizer>> gestureRecognizers = {
+    Factory(() => EagerGestureRecognizer())
+  };
+
+  UniqueKey _key = UniqueKey();
+
   Widget _page = Placeholder();
   int _selectedIndex = 0;
+  bool _isLoading = true;
+  List<articleInfo> resources = [articleInfo("https://www.indeed.com/career-advice/interviewing/how-to-prepare-for-an-interview", "How to Prepare for an Interview", "Indeed", "assets/images/indeedResource.jpg"),];
+  Map<int,bool> _isFavorited = <int,bool>{};
 
   handlePageChange(index) {
     this.widget.presenter.updatePage(index);
@@ -32,6 +65,7 @@ class _ResourcesPageState extends State<ResourcesPage> implements ResourcesView 
   void updatePage(Widget page){
     setState(() {
       _page = page;
+      _isLoading = false;
     });
   }
 
@@ -93,8 +127,91 @@ class _ResourcesPageState extends State<ResourcesPage> implements ResourcesView 
   @override
   Container ResourcePage(){
     return Container(
-      child: Text('This will be the resources page'),
+    //child: Text('This will be the locations page'),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Resources to Help You Prepare For Your Interview"),
+              SizedBox(height: 10.0,),
+            ]
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 9,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: createArticleRows(),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      )
     );
+  }
+
+
+  List<Widget> createArticleRows() {
+    return List.generate(resources.length, (index) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(padding: EdgeInsets.all(10.0)),
+          Flexible(
+            fit: FlexFit.tight,
+            flex: 3,
+            child: Row(
+              children: [
+                InkWell(
+                  child: Image.asset(resources[index].imageAsset, width: 100,),
+                  onTap: () => showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return InAppWebView(
+                        key: _key,
+                        gestureRecognizers: gestureRecognizers,
+                        initialUrlRequest: URLRequest(
+                        url: WebUri.uri(Uri.parse(resources[index].URL)),
+                        ),
+                        onWebViewCreated: (InAppWebViewController controller){
+                          inAppWebViewController = controller;
+                        }
+                      );
+                    }
+                  ),
+                ),
+                SizedBox(width: 10,),
+                Flexible(
+                child: Column(
+                  //mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                      Text(resources[index].articleTitle, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                      Text(resources[index].website, style: TextStyle(fontSize: 15,),),
+                  ],
+                ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              IconButton(
+                onPressed: (){
+                  //handleFavorite(index, locations, dataType);
+                  //handlePageChange(_selectedIndex);
+                },
+                icon: Icon((_isFavorited[index] == true ? Icons.favorite : Icons.favorite_border), color: Colors.deepPurple.shade700, size: 30.0,),
+              ),
+            ],
+          ),
+        ],
+      );
+    });
   }
 
   @override
