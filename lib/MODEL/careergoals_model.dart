d4f10851925213b31a20a6e82104ff60fa947241
Andrew Martini <mart6482@d.umn.edu>
2025-05-03 15:30:30 -0500

    added map related functions for firebase integration

--- lib/MODEL/careergoals_model.dart
+++ lib/MODEL/careergoals_model.dart
@@ -8,7 +8,7 @@ class CareerGoal{
   final GoalType goalType;
   final DateTime completionDate;
   final bool isCompleted;
-  final DateTime? createdAt;
+  final DateTime? creationDate;
 
 
   CareerGoal({
@@ -17,11 +17,36 @@ class CareerGoal{
     required this.goalType,
     required this.completionDate,
     this.isCompleted = false,
-    this.createdAt,
+    this.creationDate,
   });
 
+  Map<String, dynamic> toMap() {
+    return {
+      'goal': goal,
+      'goalType': goalType.toString(),
+      'completionDate': completionDate.toIso8601String(),
+      'isCompleted': isCompleted,
+      'createdAt': creationDate?.toIso8601String(),
+    };
 
+  }
 
+  factory CareerGoal.fromMap(Map<String, dynamic> map, String id) {
+      if(!map.containsKey('goal') || !map.containsKey('goalType')
+          || !map.containsKey('completionDate') || !map.containsKey('isCompleted')
+          || !map.containsKey('createdAt')){
+        throw Exception('Invalid career goal data');
+      }
+
+      return CareerGoal(
+        id: id,
+        goal: map['goal'] ?? '',
+        goalType: GoalType.values.firstWhere((type) => type.toString() == map['goalType']),
+        completionDate: DateTime.parse(map['completionDate']),
+        isCompleted: map['isCompleted'],
+        creationDate: map['createdAt'] != null ? DateTime.parse(map['createdAt']) : null,
+      );
+  }
 
 }
 
