07733d9f9baee0638fd23808a351f93c101e901b
etzko009 <etzko009@d.umn.edu>
2025-05-05 14:22:44 -0500

    adding the favorite icons and functionality to the software search page

--- lib/VIEW/SOFTWARE_ENG/software_job_search_tab.dart
+++ lib/VIEW/SOFTWARE_ENG/software_job_search_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../MODEL/data_read.dart';
+import '../../PRESENTER/softwareJobs_presenter.dart';
 
 /*
 to search for a job title from the database and see the company name, location,
@@ -18,11 +19,32 @@ class JobSearchTab extends StatefulWidget{
 
 class _JobSearchTabState extends State<JobSearchTab> {
   late List<Job> filteredJobs;
+  Map<Job,int> originalIndex = <Job,int>{};
+  Map<int,bool> _isFavorited = <int,bool>{};
+  bool _isLoading = true;
+
+  late SoftwareJobsPresenter presenter;
 
   @override
   void initState() {
     super.initState();
     filteredJobs = widget.allJobs;
+
+    int index = 0;
+    for(Job j in widget.allJobs){
+      originalIndex[j] = index;
+      index++;
+    }
+
+    presenter = SoftwareJobsPresenter();
+    initData();
+  }
+
+  void initData() async {
+    _isFavorited = await presenter.setMaps();
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   void _filterJobs(String query) {
@@ -42,39 +64,55 @@ class _JobSearchTabState extends State<JobSearchTab> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(12),
-          // TEXT FIELD (SEARCH BAR)
-          child: TextField(
-            onChanged: _filterJobs,
-            decoration: InputDecoration(
-              hintText: 'Search job title, company name, location, salary...',
-              border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
-              suffixIcon: Icon(Icons.search),
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12),
+            // TEXT FIELD (SEARCH BAR)
+            child: TextField(
+              onChanged: _filterJobs,
+              decoration: InputDecoration(
+                hintText: 'Search job title, company name, location, salary...',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8)),
+                suffixIcon: Icon(Icons.search),
+              ),
             ),
           ),
-        ),
 
-        // DISPLAY JOB INFORMATION
-        Expanded(
-          child: ListView.builder(
-            itemCount: filteredJobs.length,
-            itemBuilder: (_, i){
-              final job = filteredJobs[i];
-              return ListTile(
-                title: Text(job.title),
-                subtitle: Text('${job.company} • ${job.location}'),
-                trailing: Text('\$${job.avgSalary}'),
-              );
-            }
+          // DISPLAY JOB INFORMATION
+          Expanded(
+              child: _isLoading ? Center(child: CircularProgressIndicator()) : ListView.builder(
+                  itemCount: filteredJobs.length,
+                  itemBuilder: (_, i) {
+                    final job = filteredJobs[i];
+                    return ListTile(
+                      //title: Text(job.title),
+                      title: Text('${job.title} • \$${job.avgSalary}'),
+                      subtitle: Text('${job.company} • ${job.location}'),
+                      //trailing: Text('\$${job.avgSalary}'),
+                      trailing: IconButton(
+                        onPressed: () {
+                          updateData() async {
+                            _isFavorited = await presenter.updateFavoriteData(
+                                originalIndex[job]!, job);
+                          }
+
+                          setState(() {
+                            updateData();
+                          });
+                        },
+                        icon: Icon(
+                          (_isFavorited[originalIndex[job!]] == true ? Icons.favorite : Icons
+                              .favorite_border), color: Colors.green.shade700,
+                          size: 30.0,),
+                      ),
+                    );
+                  }
+              )
           )
-        )
 
 
-      ],
-    );
-  }
-
-
+        ],
+      );
+    }
 }
