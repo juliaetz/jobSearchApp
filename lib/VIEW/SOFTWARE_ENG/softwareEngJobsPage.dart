fa52674f96044cae438f30c8fd3bb190c21a3216
dange084 <dange084@d.umn.edu>
2025-05-04 20:16:17 -0500

    added search bar to software eng page!

--- lib/VIEW/SOFTWARE_ENG/softwareEngJobsPage.dart
+++ lib/VIEW/SOFTWARE_ENG/softwareEngJobsPage.dart
@@ -1,7 +1,9 @@
 import 'package:final_project/PRESENTER/load_data.dart';
 import 'package:flutter/material.dart';
 import '../../MODEL/data_read.dart';
+import 'software_job_search_tab.dart';
 
+int _selectedIndex = 0;
 
 class SoftwareEngJobsPage extends StatefulWidget{
   @override
@@ -27,14 +29,21 @@ class _SoftwareEngJobsPageState extends State<SoftwareEngJobsPage>{
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // later: build ListView, add search/filter, compare by city, …
-    return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.green, title: Text('Software Engineering Careers')),
-      body: ListView.builder(
+
+  void _onItemTapped(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _buildBody() {
+    if(_selectedIndex == 2){
+      return JobSearchTab(allJobs: jobs);
+    }
+    else {
+      return ListView.builder(
         itemCount: jobs.length,
-        itemBuilder: (_, i) {
+        itemBuilder: (_, i){
           final j = jobs[i];
           return ListTile(
             title: Text(j.title),
@@ -42,15 +51,27 @@ class _SoftwareEngJobsPageState extends State<SoftwareEngJobsPage>{
             trailing: Text('\$${j.avgSalary}'),
           );
         },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
+      );
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    // later: build ListView, add search/filter, compare by city, …
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.green, title: Text('Software Engineering Careers')),
+
+    body: _buildBody(),
+
+    bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.list), label: 'List'),
           BottomNavigationBarItem(icon: Icon(Icons.filter_list), label: 'Filter'),
           BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
         ],
-        //currentIndex: _selectedIndex,
-        //onTap: _onItemTapped,
       ),
 
     );
