774be3df54494e22b18fe95b0ccbebf6630d6e50
ols00068 <ols00068@d.umn.edu>
2025-05-06 00:58:42 -0500

    Removed redundant list view

--- lib/VIEW/SOFTWARE_ENG/softwareEngJobsPage.dart
+++ lib/VIEW/SOFTWARE_ENG/softwareEngJobsPage.dart
@@ -16,7 +16,6 @@ class _SoftwareEngJobsPageState extends State<SoftwareEngJobsPage> {
   List<Job> jobs = [];
   String? loadError;
   int _selectedIndex = 0;
-  Map<int, bool> _isFavorited = <int,bool>{};
 
   late SoftwareJobsPresenter presenter;
 
@@ -25,17 +24,11 @@ class _SoftwareEngJobsPageState extends State<SoftwareEngJobsPage> {
     super.initState();
 
     presenter = SoftwareJobsPresenter();
-    initData();
 
     repo.loadAndSort()
         .then((list) => setState(() => jobs = list))
         .catchError((e) => setState(() => loadError = e.toString()));
   }
-
-  void initData() async {
-    _isFavorited = await presenter.setMaps();
-  }
-
   void _onItemTapped(int index) {
     setState(() => _selectedIndex = index);
   }
@@ -51,35 +44,10 @@ class _SoftwareEngJobsPageState extends State<SoftwareEngJobsPage> {
     } else {
       // Define your tabs here:
       final tabs = <Widget>[
-        // 1) List view
-        ListView.builder(
-          itemCount: jobs.length,
-          itemBuilder: (_, i) {
-            final j = jobs[i];
-            return ListTile(
-              title: Text('${j.title} • \$${j.avgSalary}'),
-              subtitle: Text('${j.company} • ${j.location}'),
-              //trailing: Text('\$${j.avgSalary}'),
-              trailing: IconButton(
-                onPressed: (){
-                  updateData() async{
-                    _isFavorited = await presenter.updateFavoriteData(i, jobs[i]);
-                  }
-
-                  setState(() {
-                    updateData();
-                  });
-                },
-                icon: Icon((_isFavorited[i] == true ? Icons.favorite : Icons
-                .favorite_border), color: Colors.green.shade700, size: 30.0,),
-              ),
-            );
-          },
-        ),
-        // 2) Filter by location/company
-        FilterJobsInSoftwareView(),
-        // 3) Search
+        // 1) Search
         SoftEngJobSearchTab(allJobs: jobs),
+        // 2) Filter by city
+        FilterJobsInSoftwareView(),
       ];
       content = tabs[_selectedIndex];
     }
@@ -94,9 +62,8 @@ class _SoftwareEngJobsPageState extends State<SoftwareEngJobsPage> {
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'List'),
-          BottomNavigationBarItem(icon: Icon(Icons.filter_list), label: 'Filter'),
           BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.filter_list), label: 'Filter'),
         ],
       ),
     );
